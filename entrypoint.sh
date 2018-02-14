c85b9065e18032559a40d68ecc56fb8c0d600a5a
Aaronstar-McClure <jfeldt19@gmail.com>
2018-02-14 12:15:40 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@ sleep 5
 
 cd /home/container
 
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
